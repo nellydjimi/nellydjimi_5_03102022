aadb1373d9ba036af5ff03ff8e36428b955affde
nellydjimi <djiminelly@gmail.com>
2022-11-28 23:40:30 +0100

    test

--- front/script/cart.js
+++ front/script/cart.js
@@ -3,10 +3,11 @@ main();
 function main() {
     displayCart();
     form();
+    deleteCanap();
 }
 
 function displayCart() {
-    const monPanier = JSON.parse(localStorage.getItem('monPanier'));
+    let monPanier = JSON.parse(localStorage.getItem('monPanier'));
     //boucle for
     let numberOfCanap = 0;
     let priceTotal = 0;
@@ -115,7 +116,7 @@ function displayCart() {
 
 
 function updateQuantity(canapToCheck) {
-    const monPanier = JSON.parse(localStorage.getItem('monPanier'));
+    let monPanier = JSON.parse(localStorage.getItem('monPanier'));
     var inputQuantity = document.querySelector(".itemQuantity");
     console.log(inputQuantity);
     console.log('test', inputQuantity.value);
@@ -127,104 +128,125 @@ function updateQuantity(canapToCheck) {
     displayCart();
 }
 
-function form(){
 
-    const order = document.querySelector('#order');
-    order.addEventListener('click', (e)=> {
+function deleteCanap() {
+    let deleteItem = document.querySelectorAll('.deleteItem');
+    let monPanier = JSON.parse(localStorage.getItem('monPanier'));
+    
+    for (let k = 0; k < deleteItem.length; k++){
+        deleteItem[k].addEventListener("click" , (event) => {
+            event.preventDefault();
+
+            //Selection de l'element à supprimer en fonction de son id ET sa couleur
+            let idDelete = monPanier[i].id;
+            let colorDelete = monPanier[i].colors;
+
+            monPanier = monPanier.filter( let => let.id !== idDelete || let.colors !== colorDelete );
+            
+            localStorage.setItem("monPanier", JSON.stringify(monPanier));
+            
+            //Alerte produit supprimé et refresh
+            alert("Ce produit a bien été supprimé du panier");
+            location.reload();
+            
+        })
+    }
+    }
+
+
+
+function form(){
+   const orderForm = document.querySelector('.cart__order__form');
+
+    
+    const btnOrder = document.querySelector('#order');
+    btnOrder.addEventListener('click', (event)=> {
+        event.preventDefault();
         console.log('order');
+        //RegExp
+        let firstNameRegExp= new RegExp(/^[^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{3,20}$/) ;
+        let lastNameRegExp = new RegExp (/^[^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{3,20}$/);
+        let addressRegExp = new RegExp (/^[a-zA-Z0-9\s\,\''\-]*$/);
+        let cityRegExp = new RegExp (/^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/);
+        let emailRegExp = new RegExp (/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+
+//recupère id formulaire
         const customer = {
             firstName : document.getElementById('firstName').value,
             lastName : document.getElementById('lastName').value,
-            adress : document.getElementById('address').value,
+            address : document.getElementById('address').value,
             city : document.getElementById('city').value,
             email : document.getElementById('email').value,
         }
+
         console.log('customer :', customer)
-        fetch("http://localhost:3000/api/order", { method: "POST", body: JSON.stringify({contact: customer, products: JSON.parse(localStorage.getItem('monPanier'))})});
+        const canapsId = [];
+        
+        JSON.parse(localStorage.getItem('monPanier')).forEach((canap) => {
+            canapsId.push(canap.id);
+        });
+
+        let formIsCorrect = true;
+        //check firstName
+        if (firstNameRegExp.test(customer.firstName) === formIsCorrect) { 
+            return true;
+        } 
+        else {
+            let firstNameErrorMsg = document.querySelector('#firstNameErrorMsg');
+            firstNameErrorMsg.innerText = "Prénom invalide";
+        }
+
+        //check lastName
+        if (lastNameRegExp.test(customer.lastName)=== formIsCorrect) { 
+            return true;
+        } else {
+            let lastNameErrorMsg = document.querySelector('#lastNameErrorMsg');
+            lastNameErrorMsg.innerText = "Nom invalide";
+        }
+
+        //check address
+        if (addressRegExp.test(customer.address)=== formIsCorrect ) { 
+            return true;
+        } else {
+            let addressErrorMsg = document.querySelector('#addressErrorMsg');
+            addressErrorMsg.innerText = "Adresse invalide";
+        }
+
+        //check city
+        if (cityRegExp.test(customer.city)=== formIsCorrect) { 
+            return true;
+        } else {
+            let cityErrorMsg = document.querySelector('#cityErrorMsg');
+            cityErrorMsg.innerText = "Ville invalide";
+        }
+
+        //check email
+        if (emailRegExp.test(customer.email)=== formIsCorrect) { 
+            return true ;
+        } else {
+            let emailErrorMsg = document.querySelector('#emailErrorMsg');
+            emailErrorMsg.innerText = "Email invalide"; 
+        }
+
+
+        if (formIsCorrect === true) {
+            fetch("http://localhost:3000/api/products/order", { 
+                method: "POST", 
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({contact: customer, products: canapsId})
+            }).then( (response) =>{
+                
+            
+            })
+            //rediriger vers la page cofirmation.html
+        }
     })
-/*
-    order.addEventListener('click', (event)=>{
-    console.log('test');
-    //recupère id formulaire
-    const customer ={
-    firstName : document.getElementById('firstName').type,
-    lastName : document.getElementById('lastName').type,
-    adress : document.getElementById('adress').type,
-    city : document.getElementById('city').type,
-    email : document.getElementById('email').type,
-    }
-
-    //validation firstName
-
-    function checkFirstName (){
-    if (firstName = customer.type ){
-        return true
-    }
-    else{
-        let firstNameErrorMsg = document.getElementById('firstNameErrorMsg');
-        alert ('Prenom non valide');
-    }}
-    //validation lastName
-function checkLastName(){
-    if (lastName = customer.type  ){
-        return true
-    }
-    else{
-        let lastNameErrorMsg = document.getElementById('lastNameErrorMsg');
-       alert ('Nom non valide') ;
-    }}
-
-    //validation adress
-    function checkAdress(){
-    if (adressName = customer.type  ){
-        return true
-    }
-    else{
-        let adressErrorMsg = document.getElementById('adressErrorMsg');
-       alert ('Adresse non valide');
-    }}
-
-    //validation city
-    function checkCity(){
-    if (city = customer.type ){
-        return true
-    }
-    else{
-        let cityErrorMsg = document.getElementById('cityErrorMsg');
-       alert('Ville non valide') ;
-    }}
-
-    //validation email
-    function checkEmail(){
-    if (email = customer.type  ){
-        return true
-    }
-    else{
-       let emailErrorMsg = document.getElementById('emailErrorMsg');
-       alert ('Adresse mail non conforme');
-    }}
-    
-})*/
-
     
 }
 
-  
-  
-    
-   
-
-   
-    
-   
-
-    
-   
-
-   
-    
-   
-   
 
 
 
@@ -232,30 +254,3 @@ function checkLastName(){
 
 
 
-
-
-
-/*function delete (){
-const deleteArticle = document.querSelector(".deleteItem");
-deleteArticle.forEach ((p) =>{
-    p.addEvenListener ( 'click', e=>{
-        deleteItem (e, items)
-    
-    )}
-}
-
-function deleteItem (e){
-    let items =
-localstorage.setItems( JSON.stringify(items))
-if (item.lenght == 0){
-    localStorage.removeItems
-}
-
-
-
-
-
-
-
-
-}*/
